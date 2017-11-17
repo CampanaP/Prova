02cad382df52adf0ba6b3e1dbf6a1529989ecc4f
CampanaP <33429416+CampanaP@users.noreply.github.com>
2017-11-17 16:18:47 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,8 +1,20 @@
+<?php
+	if(!isset($_COOKIE["contatore"]))
+	{
+		setcookie("contatore",'1',time()+(86400*7),"/");
+		print("Benvenuto, questa è la prima volta che entri in questo sito.");
+	}
+	else
+	{
+		$conta=$_COOKIE["contatore"]+1;
+		setcookie("contatore",$conta,time()+(86400*7),"/");
+		print("Hai visitato questo sito ".$conta."volte.");
+	}
+?>
 <html>
- <head>
-  <title>Test PHP</title>
- </head>
- <body>
- <?php echo "Hello World!<p>"; ?>
- </body>
+	<head>
+	<title>Compito </title>
+	</head>
+	<body>
+	</body>
 </html>
